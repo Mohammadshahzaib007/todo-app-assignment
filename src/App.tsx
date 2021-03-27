7464a28551a656ce7054bea1dce9ead2236bab10
Mohammad Shahzaib <shahzaibmohammad22@gmail.com>
2021-03-27 21:16:49 +0530

    routing animation was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, withRouter } from "react-router";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Layout from "./components/Layout/Layout";
 import AllTasks from "./pages/AllTasks";
 import CompletedTasks from "./pages/CompletedTasks";
 import PendingTasks from "./pages/PendingTasks";
 
-function App() {
-  return (
-    <Layout>
+// for animating routes
+const AnimatedSwitch = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition key={location.key} classNames="fade" timeout={1000}>
       <Switch>
         <Route exact path="/" component={AllTasks} />
         <Route path="/pending-tasks" component={PendingTasks} />
         <Route path="/completed-tasks" component={CompletedTasks} />
       </Switch>
-    </Layout>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+function App() {
+  return (
+    <div style={{ display: "relative" }}>
+      <Layout>
+        <AnimatedSwitch />
+      </Layout>
+    </div>
   );
 }
 
